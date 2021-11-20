1f0e12f1622e4a11069b95ccb2c3ead1cc33c5ae
yxnzhe <71542921+yxnzhe@users.noreply.github.com>
2021-11-21 05:55:35 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -71,7 +71,8 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         source = check_file(source)  # download
 
     # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    save_dir = Path(project)
+#     save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Load model
